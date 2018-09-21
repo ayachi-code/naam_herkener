fbc48cddcd27520178418208c7abd91a89a4b9b6
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-09-21 16:35:23 +0200

    foutmelding word ook weer gegeven

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -8,6 +8,6 @@ let input_naam = document.getElementById("input_naam").addEventListener("keypres
 function zoeken_naam(naam /*"test: bilal " */) {
     fetch("https://gender-api.com/get?name=Diana&key=jEdttMdAylfxkHbzQc")
         .then()
-        .catch((error) => console.log("Er is iets fout gegaan ... foutmelding"));
+        .catch((error) => console.log("Er is iets fout gegaan ... foutmelding: " + error));
 
 }
