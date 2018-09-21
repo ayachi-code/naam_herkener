fcfcb695116ff6f81764b3c46d4edf87773c0092
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-09-21 16:26:50 +0200

    event listener gemaakt als enter word geklikt en dan hey uitprint

---
+++ public/javascript/index.js
@@ -0,0 +1,6 @@
+let input_naam = document.getElementById("input_naam").addEventListener("keypress",(event) => {
+    let keya = event.keyCode;
+    if (keya == 13) {
+        console.log("hey")
+    }
+})
