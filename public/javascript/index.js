eb02442ed7646c78bcf1a18e05cdf295c40b83cf
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-09-21 16:56:48 +0200

    ander component land tgv

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -3,7 +3,7 @@ Bilal el Ayachi
 website: bilalayachi.nl
 */
 
-
+//funtie input naam
 let input_naam = document.getElementById("input_naam").addEventListener("keypress",(event) => {
     let keya = event.keyCode;
     if (keya == 13) {
@@ -14,6 +14,8 @@ let input_naam = document.getElementById("input_naam").addEventListener("keypres
 function zoeken_naam(naam /*"test: bilal " */) {
     fetch("https://gender-api.com/get?name=Diana&key=jEdttMdAylfxkHbzQc")
         .then((resultaat) => resultaat.json())
+        .then((data) => { 
+            return console.log(data.name)
+        })
         .catch((error) => console.log("Er is iets fout gegaan ... foutmelding: " + error));
-        
 }
