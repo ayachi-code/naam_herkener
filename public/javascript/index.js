c824284bc209d40ec5ce7b122c33eb1d34718cb8
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-09-21 16:44:34 +0200

    nam lander herkender word het nu genoemd

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,3 +1,9 @@
+/* 
+Bilal el Ayachi
+website: bilalayachi.nl
+*/
+
+
 let input_naam = document.getElementById("input_naam").addEventListener("keypress",(event) => {
     let keya = event.keyCode;
     if (keya == 13) {
@@ -7,7 +13,7 @@ let input_naam = document.getElementById("input_naam").addEventListener("keypres
 
 function zoeken_naam(naam /*"test: bilal " */) {
     fetch("https://gender-api.com/get?name=Diana&key=jEdttMdAylfxkHbzQc")
-        .then((resultaat) => resultaat.json() )
+        .then((resultaat) => resultaat.json())
         .catch((error) => console.log("Er is iets fout gegaan ... foutmelding: " + error));
-
+        
 }
