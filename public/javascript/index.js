4bd57727f0c57a461e346f9fce3d325d5dd7b10f
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-09-21 16:32:05 +0200

    fetch url gegeven

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -6,5 +6,6 @@ let input_naam = document.getElementById("input_naam").addEventListener("keypres
 })
 
 function zoeken_naam(naam /*"test: bilal " */) {
-    
+    fetch("https://gender-api.com/get?name=Diana&key=jEdttMdAylfxkHbzQc");
+
 }
